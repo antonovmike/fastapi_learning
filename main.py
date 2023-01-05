92d0fbb612aa87604ff6a20b75ff3b7522cdce94
Antonov Mike <101407994+antonovmike@users.noreply.github.com>
2023-01-05 18:36:36 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 
@@ -9,10 +10,19 @@ app = FastAPI()
 #     html_content = "<h2>Hello FastAPI</h2>"
 #     return HTMLResponse(content=html_content)
 
+# Data serialization v1
+# @app.get("/")
+# def root():
+#     data = {"message": "Hello FastAPI"}
+#     json_data = jsonable_encoder(data)
+#     return JSONResponse(content=json_data)
+
+# Data serialization v2
+# If we need to send some custom non-serializable data, or if we
+# need custom  serialization, we can define our own serializer
 @app.get("/")
 def root():
-    return {"message": "Hello FastAPI"}
-
+    return JSONResponse(content={"message": "Hello FastAPI"})
 
 @app.get("/about")
 def about():
