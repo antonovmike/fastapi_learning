68ce334217fd25ed2dae9ae05f673202f51847bb
antonovmike <antonvma@outlook.com>
2023-01-05 19:18:19 +0500

    User validation

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def admin():
     return {"message": "Hello admin"}
 
 # Second
-@app.get("/users/{name}")
-def users(name: str = Path(min_length=3, max_length=9)):
-    return {"name": name}
+@app.get("/users/{name}/{age}")
+def users(name: str = Path(min_length=3, max_length=9), age: int = Path(ge=22, lt=111)):
+    return {"name": name, "age": age}
 
