275e7dd3c93b0321a35db81ad6cbc9543a1076d8
antonovmike <antonvma@outlook.com>
2023-01-05 18:22:49 +0500

    added /documentation

--- main.py
+++ main.py
@@ -17,3 +17,9 @@ def root():
 @app.get("/about")
 def about():
     return {"message": "FastAPI framework, high performance, easy to learn, fast to code, ready for production"}
+
+
+@app.get("/documentation")
+def about():
+    return {"message": "Documentation: https://fastapi.tiangolo.com"}
+
