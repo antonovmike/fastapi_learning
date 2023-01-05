df4b8f6140d62c3ad9469cdbb8ab16bc36cfa0c1
antonovmike <antonvma@outlook.com>
2023-01-05 19:13:00 +0500

    Path queue

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/users/{name}/{age}")
-def users(name, age):
-    return {"user_name": name, "user_age": age}
+# First
+@app.get("/users/admin")
+def admin():
+    return {"message": "Hello admin"}
+
+# Second
+@app.get("/users/{name}")
+def users(name):
+    return {"user_name": name}
