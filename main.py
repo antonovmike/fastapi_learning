856361130db643af0c6864b801b5e2b41814dba3
antonovmike <antonvma@outlook.com>
2023-01-06 14:19:52 +0500

    validation

--- main.py
+++ main.py
@@ -9,9 +9,18 @@ def root():
     return FileResponse("public/index.html")
 
 
+# @app.post("/hello")
+# def hello(data=Body()):
+#     name = data["name"]
+#     age = data["age"]
+#     return {"message": f"{name}, your age is {age}"}
+
+
+# @app.post("/hello")
+# def hello(name = Body(embed=True), age = Body(embed=True)):
+#     return {"message": f"{name}, your age is {age}"}
+
+# Validation
 @app.post("/hello")
-# def hello(name = Body(embed=True)):
-def hello(data=Body()):
-    name = data["name"]
-    age = data["age"]
-    return {"message": f"{name}, your age is - {age}"}
+def hello(name: str = Body(embed=True, min_length=3, max_length=9), age: int = Body(embed=True, ge=18, lt=111)):
+    return {"message": f"{name}, your age is {age}"}
