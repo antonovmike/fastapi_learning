ec5aabe605662e302e4f1eb234f0bb6af6d91dbf
antonovmike <antonvma@outlook.com>
2023-01-06 14:53:53 +0500

    List of values

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from pydantic import BaseModel, Field
 
 
 class Person(BaseModel):
-    name: str = Field(default="Undefined", min_length=3, max_length=20)
+    name: str = Field(default="Undefined", min_length=3, max_length=9)
     age: int = Field(default=18, ge=18, lt=111)
+    languages: list = Field(default="Undefined")
 
 
 app = FastAPI()
@@ -18,4 +19,4 @@ def root():
 
 @app.post("/hello")
 def hello(person: Person):
-    return {"message": f"Hello, {person.name}, your age is {person.age}"}
+    return {"message": f"Hello, {person.name}, your age is {person.age}. Languages: {person.languages}"}
